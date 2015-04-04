49b892faa3c7cb19a31c77f1410f22aa0541c94a
roof <roofimon@gmail.com>
2015-04-04 16:59:10 +0700

    First Commit

---
+++ Captcha/Captcha.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptchaLibrary {
+    public class Captcha {
+        private int pattern;
+        private int leftOperand;
+        private int rightOperand;
+        private int oper;
+        Dictionary<int, string> numberStringMapping = new Dictionary<int, string> 
+        {
+                {1, "One"}, 
+                {2, "Two"},
+                {3, "Three"},
+                {4, "Four"},
+                {5, "Five"},
+                {6, "Six"},
+                {7, "Seven"},
+                {8, "Eight"},
+                {9, "Nine"},
+       };
+       
+
+        public Captcha(int pattern, int leftOperand, int oper, int rightOperand) {
+            this.pattern = pattern;
+            this.leftOperand = leftOperand;
+            this.oper = oper;
+            this.rightOperand = rightOperand;
+        }
+
+        public string LeftOperand() {
+            if (isNumberStringPattern()) {
+                return this.leftOperand.ToString();
+            }
+            return numberStringMapping[this.leftOperand];
+        }
+
+        private bool isNumberStringPattern() {
+            return this.pattern == 1;
+        }
+
+        public string RightOperand() {
+            if (isNumberStringPattern()) {
+                return numberStringMapping[this.rightOperand];
+            }
+            return this.rightOperand.ToString();
+        }
+
+        public string Operator() {
+            Dictionary<int, string> oper = new Dictionary<int, string> {
+                {1, "+"},
+                {2, "*"},
+                {3, "-"},
+            };
+            return oper[this.oper];
+        }
+    }
+}
---
+++ CaptchaTest/CaptchaTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CaptchaLibrary;
+
+namespace CaptchaTest {
+    [TestClass]
+    public class CaptchaTest {
+        [TestMethod]
+        public void Operator_WhenInputIs2118_ShouldBePlus() {
+            Captcha captcha = new Captcha(2, 1, 1, 8);
+            String oper = captcha.Operator();
+            Assert.AreEqual("+", oper);
+        }
+
+        [TestMethod]
+        public void Operator_WhenInputIs2128_ShouldBeMultiply() {
+            Captcha captcha = new Captcha(2, 1, 2, 8);
+            String oper = captcha.Operator();
+            Assert.AreEqual("*", oper);
+        }
+
+        [TestMethod]
+        public void Operator_WhenInputIs2138_ShouldBeMultiply() {
+            Captcha captcha = new Captcha(2, 1, 3, 8);
+            String oper = captcha.Operator();
+            Assert.AreEqual("-", oper);
+        }
+
+        [TestMethod]
+        public void RightOperand_WhenInputIs2118_ShouldBe8() {
+            Captcha captcha = new Captcha(2, 1, 1, 8);
+            String operand = captcha.RightOperand();
+            Assert.AreEqual("8", operand);
+        }
+
+        [TestMethod]
+        public void RightOperand_WhenInputIs1118_ShouldBeEight() {
+            Captcha captcha = new Captcha(1, 1, 1, 8);
+            String operand = captcha.RightOperand();
+            Assert.AreEqual("Eight", operand);
+        }
+
+        [TestMethod]
+        public void RightOperand_WhenInputIs1119_ShouldBeNine() {
+            Captcha captcha = new Captcha(1, 1, 1, 9);
+            String operand = captcha.RightOperand();
+            Assert.AreEqual("Nine", operand);
+        }
+
+        [TestMethod]
+        public void LeftOperand_WhenInputIs2199_ShouldBeOne() {
+            Captcha captcha = new Captcha(2, 1, 9, 9);
+            String operand = captcha.LeftOperand();
+            Assert.AreEqual("One", operand);
+        }
+
+        [TestMethod]
+        public void LeftOperand_WhenInputIs2911_ShouldBeOne() {
+            Captcha captcha = new Captcha(2, 9, 1, 1);
+            String operand = captcha.LeftOperand();
+            Assert.AreEqual("Nine", operand);
+        }
+
+        [TestMethod]
+        public void LeftOperand_WhenInputIs1199_ShouldBe1() {
+            Captcha captcha = new Captcha(1, 1, 9, 9);
+            String operand = captcha.LeftOperand();
+            Assert.AreEqual("1", operand);
+        }
+
+        [TestMethod]
+        public void LeftOperand_WhenInputIs1299_ShouldBe2() {
+            Captcha captcha = new Captcha(1, 2, 9, 9);
+            String operand = captcha.LeftOperand();
+            Assert.AreEqual("2", operand);
+        }
+
+        [TestMethod]
+        public void LeftOperand_WhenInputIs1999_ShouldBe9() {
+            Captcha captcha = new Captcha(1, 9, 1, 1);
+            String operand = captcha.LeftOperand();
+            Assert.AreEqual("9", operand);
+        }
+    }
+}
